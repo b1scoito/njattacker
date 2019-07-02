bb0bcd7f32bda137b4567077ddf5898eb18f26d8
Rafael Felix <b1scoitorg@gmail.com>
2019-07-02 11:47:07 -0300

    i don't really know what i changed

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-var net = require('net');
-var config = require('./config.json');
-var rndconfig = require('./config_random.json');
 if (process.argv.length < 6 || process.argv.length > 6) {
     console.log("Can't attack please make sure to execute: node app.js ip:port count delay mode ex node app.js 192.233.122.21:1177 100 15 config, mode could be either config or random, count could be infinite string.");
     process.exit(0);
 }
+var net = require('net');
+var config = require('./config.json');
+var rndconfig = require('./config_random.json');
 var payload;
 var ip = process.argv[2].split(':')[0];
 var port = process.argv[2].split(':')[1];
 let i = 0;
-let bru = 0;
+let countval = 0;
 
 function choose(choices) {
     var index = Math.floor(Math.random() * choices.length);
     return choices[index];
 }
 
-function next() {
+function delayedLoop() {
     if (process.argv[3] === "infinite") {
-        bru = i + 1;
+        countval = i + 1;
     } else {
-        bru = process.argv[3];
+        countval = process.argv[3];
     }
-    if (i < bru) {
-        i++
+    if (i < countval) {
+        i++;
         setTimeout(() => {
             createFakeClient();
-            next();
-        }, process.argv[4])
+            delayedLoop();
+        }, process.argv[4]);
     }
 }
-next()
+delayedLoop();
 
 function createFakeClient() {
     const socket = net.createConnection(port, ip, () => {
@@ -41,15 +41,15 @@ function createFakeClient() {
             case 'random':
                 payload = `ll|'|'|${Buffer.from(choose(rndconfig.random_victim_string_prefixes)).toString('base64')}|'|'|${choose(rndconfig.username) + "-PC"}|'|'|${choose(rndconfig.username)}|'|'|${config.install_time}|'|'||'|'|${choose(["MacOS", "Linux", "Windows"])} ${choose(["XP", "7", "8", "8.1", "10"])} ${choose(["Home Premium", "Pro", "Professional", "Ultimate", "Enterprise"])} x69|'|'|${choose(["Yes", "No", "Maybe", "Talvez."])}|'|'|${choose(["0.0d", "0.1d", "0.2d", "0.3d", "0.4d", "0.5d", "0.6d", "0.7d", "0.8d", "0.9d", "0.10d"])}|'|'|..|'|'|${Buffer.from(choose(rndconfig.foreground)).toString('base64')}|'|'|`;
                 break;
-                default:
-                    console.log("Wrong mode");
-                    process.exit(0);
+            default:
+                console.log("Wrong mode");
+                process.exit(0);
         }
         var complete_payload = payload.length + '\x00' + payload;
         console.log(`(${i}) Attacking with Payload: ` + complete_payload + "\n");
         socket.write(complete_payload);
     });
-    socket.on('error', (data) => {
+    socket.on('error', () => {
         console.log("Connection was Closed Unexpectedly or was closed already, closing spammer.");
         process.exit(0);
     })
