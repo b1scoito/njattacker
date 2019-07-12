e3e621a3c4e78cd537a21aa1fabd858335e86593
Rafael Felix <b1scoitorg@gmail.com>
2019-07-12 16:50:02 -0300

    made changes to logs

--- log.js
+++ log.js
@@ -1,18 +1,15 @@
 const chalk = require('chalk');
 module.exports = {
     debug: function (text, thread) {
-        var today = new Date();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        return console.log(`[ ${chalk.cyan(time)} ] ${chalk.yellow("DEBUG")} [${chalk.magenta(thread)}] -> ${text}`);
+        var today = new Date(), time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        return console.log(`[ ${chalk.hex('#15dfff')(time)} ] ${chalk.hex('#ffff00')("DEBUG")} [${chalk.hex('#ff00ff')(thread)}] -> ${text}`);
     },
     info: function (text) {
-        var today = new Date();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        return console.log(`[ ${chalk.cyan(time)} ] ${chalk.green("INFO")} -> ${text}`);
+        var today = new Date(), time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        return console.log(`[ ${chalk.hex('#15dfff')(time)} ] ${chalk.hex('#00ff00')("INFO")} -> ${text}`);
     },
     error: function (text, thread, error) {
-        var today = new Date();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        return console.log(`[ ${chalk.cyan(time)} ] ${chalk.red("ERROR")} [${chalk.magenta(thread)}] on [${chalk.red(error)}] -> ${text}`);
+        var today = new Date(), time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        return console.log(`[ ${chalk.hex('#15dfff')(time)} ] ${chalk.hex.bold('#ff5555')("ERROR")} [${chalk.hex('#ff00ff')(thread)}] on [${chalk.hex('#ff5555')(error)}] -> ${text}`);
     }
 }
