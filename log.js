b7d23bdf1ff68755178c64dd046571d7c173c5be
Rafael Felix <b1scoitorg@gmail.com>
2019-07-12 18:13:37 -0300

    fix bold

--- log.js
+++ log.js
@@ -10,6 +10,6 @@ module.exports = {
     },
     error: function (text, thread, error) {
         var today = new Date(), time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        return console.log(`[ ${chalk.hex('#15dfff')(time)} ] ${chalk.hex.bold('#ff5555')("ERROR")} [${chalk.hex('#ff00ff')(thread)}] on [${chalk.hex('#ff5555')(error)}] -> ${text}`);
+        return console.log(`[ ${chalk.hex('#15dfff')(time)} ] ${chalk.bold.hex('#ff5555')("ERROR")} [${chalk.hex('#ff00ff')(thread)}] on [${chalk.hex('#ff5555')(error)}] -> ${text}`);
     }
 }
